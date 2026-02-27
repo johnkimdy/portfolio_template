b7da02c0ab05d4cbb137c598c926e4faa38b15bc
john <johnkimdy@gmail.com>
2026-02-28 08:24:36 +0900

    Change description from 'Data Scientist (at the moment)'
    more professional lingo

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Data Scientist (at the moment).
+          Data Scientist and Prudence.
           {/* I'm building{" "}
           <Link
             target="_blank"
@@ -50,3 +50,4 @@ export default function Home() {
   );
 
 }
+
